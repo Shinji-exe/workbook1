293f8f4dd38f7d80ef739359bb187e89276f543d
Timothy <timlundy74@gmail.com>
2024-10-08 10:25:40 -0400

    Initial Commit of workbook

---
+++ hello.js
@@ -0,0 +1,41 @@
+var box1 = "Hello";
+
+console.log(box1)
+
+
+//Example1 Declare and assign seperately
+var message; //declaration
+message = "Program" //assignment
+
+//Example2 Declare and assign on same line
+var message = "Programming is fun"
+
+
+
+// console.log("Jordan")
+
+// var priceRate = 12.50;
+// var hoursWorked = 45;
+
+// if(hoursWorked > 40){
+//     priceRate = priceRate * 1.5;
+// }else{
+//     priceRate
+// }
+
+// console.log(priceRate)
+
+// const object = {
+//     name: "Jordan",
+//     age: 25,
+//     young: true,
+// }
+
+// console.log(object.age)
+
+// const array = ["red", "rover", "japan", "mango"]
+
+// console.log(array)
+
+
+

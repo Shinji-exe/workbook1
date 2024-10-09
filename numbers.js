323aa1d4850f2c36e97fd129efc891c6016e7f74
Timothy <timlundy74@gmail.com>
2024-10-08 21:02:27 -0400

    More exercise

---
+++ numbers.js
@@ -0,0 +1,21 @@
+"use strict"
+
+var number1 = 10;
+var number2 = 5;
+var number3 = "100"
+
+console.log(typeof number1)
+console.log(typeof number3)
+
+var total = number1 + number2;
+console.log("total " + total)
+console.log(total + " = " + number1 + " + " + number2)
+console.log(total + " = " + `${number1}`)
+
+
+
+// var result;
+// var total;
+// var totalAmount;
+// var output;
+// var sum;
---
+++ tuesday/exercise/exercise5.js
@@ -0,0 +1,66 @@
+
+"use strict"
+
+// assets - liabilites
+var assets = 3000;
+var liabilites = 1750
+var profit;
+profit = assets - liabilites;
+
+console.log(profit)
+
+console.log("Your net-worth is " + profit)
+
+/////
+
+var length = 30;
+var width = 25;
+var area = length * width;
+console.log(area)
+
+console.log("The area of the retangle is " + area)
+
+/////
+
+var food = 20.45;
+var tipPercentage = .15;
+var totalCostAmount = food + tipAmount;
+var tipAmount = food * tipPercentage
+tipAmount = Math.round(tipAmount)
+console.log(tipAmount)
+
+console.log("The tip on a $" + food + " food bill is $" + tipAmount + "."  )
+
+var food1 = 30
+var tip = 10
+var totalCost = food1 / tip;
+console.log(totalCost)
+
+//////////
+
+
+
+
+
+////////////////
+
+//Area of Circle
+
+var radius = 5
+
+// var pi = 3.14159265359
+
+var perimeter = 2 * Math.PI * radius;
+
+var area = Math.PI * Math.pow(radius, 2)
+
+console.log("Area: " + area)
+console.log("Perimeter: " + perimeter)
+
+///////////////
+
+var numberInterest = 72;
+var interestRate = .04;
+var divi = numberInterest / interestRate ; 
+
+console.log("At a " + interestRate)
